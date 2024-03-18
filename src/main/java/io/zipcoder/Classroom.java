42b017fe43fc0125c5b96899f9d26fb4e0798f1d
Shaily Modi <shailykunal@gmail.com>
2024-03-17 22:48:41 -0400

    Finished.

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,9 +1,6 @@
 package io.zipcoder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Classroom {
     private Student[] students;
@@ -21,7 +18,7 @@ public class Classroom {
     }
 
     public Student[] getStudents() {
-        return students;
+        return this.students;
     }
 
     public Double getAverageExamScore() {
@@ -43,32 +40,84 @@ public class Classroom {
         if (studentList.size() < 30) {
             studentList.add(student);
         }
-        //students = (Student[])studentList.toArray();
-        Student[] studentsAsArray = new Student[studentList.size()];
-        studentsAsArray = studentList.toArray(studentsAsArray);
-        this.students = studentsAsArray;
+        studentList.toArray(students);
     }
 
     public void removeStudent(String firstName, String lastName) {
         ArrayList<Student> studentList = new ArrayList<Student>();
+        studentList.addAll(Arrays.asList(students));
 
         for (Student student : students) {
-            if (student.getLastName().equals(lastName) && student.getFirstName().equals(firstName)) {
+            if (student.getLastName().equals(lastName) && student.getFirstName().equals(firstName))
+            {
                 studentList.remove(student);
+                break;
             }
         }
-        students = (Student[]) studentList.toArray();
+        studentList.toArray(students);
     }
 
     public Student[] getStudentsByScore() {
-        return (Student[]) Arrays.stream(students).sorted().toArray();
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        studentList.addAll(Arrays.asList(students));
+
+        for (int i = 0; i < studentList.size(); i++)
+        {
+            for (int j = i + 1; j < studentList.size() - 1; j++)
+            {
+                if (studentList.get(j).getAverageExamScore() > studentList.get(i).getAverageExamScore())
+                {
+                    Student tmp = studentList.get(j);
+                    studentList.set(j, studentList.get(i));
+                    studentList.set(i, tmp);
+                }
+            }
+        }
+
+        return studentList.toArray(new Student[0]);
     }
 
-    public void getGradeBook() {
-//        ArrayList<Student> studentList = new ArrayList<Student>();
-//        Double maxNumber = studentList.stream().max(x,y)->
-//        {
-//            return x.getAverageExamScore(). compareTo(y.getAverageExamScore());})).get().getAverageExamScore();
-//        }
+    public Map<Student, String> getGradeBook() {
+       Map<Student, String> gradebook = new HashMap<>();
+
+       for (Student s : students)
+       {
+           if(getPercentile(s) <= 0.1)
+           {
+               gradebook.put(s, "A");
+           }
+           else if (getPercentile(s) <= 0.29)
+           {
+               gradebook.put(s, "B");
+           }
+           else if (getPercentile(s) <= 0.50)
+           {
+               gradebook.put(s, "C");
+           }
+           else if (getPercentile(s) <= 0.89)
+           {
+               gradebook.put(s, "D");
+           }
+           else if (getPercentile(s) <= 1.0)
+           {
+               gradebook.put(s, "F");
+           }
+       }
+       return gradebook;
+    }
+
+    public Double getPercentile(Student student)
+    {
+        //ArrayList<Student> studentList = new ArrayList<>(List.of(getStudentsByScore()));
+        ArrayList<Student> studentList = new ArrayList<Student>();
+        studentList.addAll(Arrays.asList(students));
+
+        int index = studentList.indexOf(student);
+        if (index == -1)
+        {
+            return 1.00;
+        }
+        return (double) (index / studentList.size());
+
     }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,8 +1,10 @@
 package io.zipcoder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class ClassroomTest
 {
@@ -48,5 +50,78 @@ public class ClassroomTest
         System.out.println(preEnrollmentAsString);
         System.out.println("===========================");
         System.out.println(postEnrollmentAsString);
+    }
+
+    @Test
+    public void removeStudentTest()
+    {
+        //Given
+        int maxNumberOfStudents = 2;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student1 = new Student("Mike", "Williams", examScores);
+        Student student2 = new Student("Jennifer", "Williams", examScores);
+
+        // When
+
+        classroom.addStudent(student1);
+        classroom.addStudent(student2);
+        Student[] preRemoval = classroom.getStudents();
+        classroom.removeStudent("Mike", "Williams");
+        Student[] postRemoval = classroom.getStudents();
+
+        // Then
+        String preRemovalAsString = Arrays.toString(preRemoval);
+        String postRemovalAsString = Arrays.toString(postRemoval);
+
+        System.out.println("===========================");
+        System.out.println(preRemovalAsString);
+        System.out.println("===========================");
+        System.out.println(postRemovalAsString);
+
+    }
+    @Test
+    public void getStudentsByScoreTest()
+    {
+        //Given
+        Double[] s1Scores = { 100.0, 150.0 };
+        Double[] s2Scores = { 225.0, 25.0 };
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        Student[] students = {s1,s2};
+
+        // When
+        Classroom classroom = new Classroom(students);
+
+        // Then
+        Assert.assertEquals(students, classroom.getStudentsByScore());
+    }
+
+    @Test
+    public void getGradeBookTest() {
+        //Given
+        Double[] s1Scores = { 100.0};
+        Double[] s2Scores = { 225.0};
+        Double[] s3Scores = { 50.0};
+        Double[] s4Scores = { 120.0};
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+        Student s3 = new Student("student", "three", s3Scores);
+        Student s4 = new Student("student", "four", s4Scores);
+
+        Student[] students = {s1, s2, s3, s4};
+        Classroom classroom = new Classroom(students);
+
+        // When
+        Map<Student, String> myMap = classroom.getGradeBook();
+
+        // Then
+        Assert.assertEquals("A", myMap.get(students[0]));
+        Assert.assertEquals("A", myMap.get(students[1]));
+        Assert.assertEquals("A", myMap.get(students[2]));
+        Assert.assertEquals("A", myMap.get(students[3]));
     }
 }
